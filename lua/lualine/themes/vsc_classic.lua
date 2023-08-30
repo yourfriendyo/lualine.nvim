864c86a171ffb05cb5eacd74f66a407dc986d4b8
yourfriendyo <3395537299@qq.com>
2023-08-30 23:14:42 +0800

    常规提交

---
+++ lua/lualine/themes/vsc_classic.lua
@@ -0,0 +1,56 @@
+-- Copyright (c) 2020-2021 shadmansaleh
+-- MIT license, see LICENSE for more details.
+-- Credit: Zoltan Dalmadi(lightline)
+-- LuaFormatter off
+
+local colors = {
+    blue        = '#569cd6',
+    green       = '#6a9955',
+    purple      = '#c586c0',
+    red1        = '#d16969',
+    yellow      = '#dcdcaa',
+    orange      = '#ce9178',
+    fg          = '#FFFFFF',
+    bg          = '#007acc',
+    bg_command  = '#9e5400',
+    bg_visual   = '#68217a',
+    bg_header   = '#2C896B',
+    bg_inactive = '#252525',
+    gray1       = '#5c6370',
+    gray2       = '#2c323d',
+    gray3       = '#3e4452',
+    foreground  = '#d4d4d4',
+}
+-- LuaFormatter on
+return {
+    normal = {
+        a = { fg = colors.fg, bg = colors.bg_header, gui = 'bold' },
+        b = { fg = colors.fg, bg = colors.bg },
+        c = { fg = colors.fg, bg = colors.bg }
+    },
+    insert = {
+        a = { fg = colors.fg, bg = colors.bg_header, gui = 'bold' },
+        b = { fg = colors.fg, bg = colors.bg },
+        c = { fg = colors.fg, bg = colors.bg },
+    },
+    visual = {
+        a = { fg = colors.fg, bg = colors.bg_visual, gui = 'bold' },
+        b = { fg = colors.fg, bg = colors.bg_visual },
+        c = { fg = colors.fg, bg = colors.bg_visual },
+    },
+    command = {
+        a = { fg = colors.fg, bg = colors.bg_command, gui = 'bold' },
+        b = { fg = colors.fg, bg = colors.bg_command },
+        c = { fg = colors.fg, bg = colors.bg_command },
+    },
+    replace = {
+        a = { fg = colors.fg, bg = colors.bg_command, gui = 'bold' },
+        b = { fg = colors.fg, bg = colors.bg_command },
+        c = { fg = colors.fg, bg = colors.bg_command },
+    },
+    inactive = {
+        a = { fg = colors.fg, bg = colors.bg_inactive },
+        b = { fg = colors.fg, bg = colors.bg_inactive },
+        c = { fg = colors.fg, bg = colors.bg_inactive },
+    }
+}
